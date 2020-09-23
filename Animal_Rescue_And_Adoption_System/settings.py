949268f8a45bfb60a3ee703aa91fc54107b94141
hsourov <shalder1@student.ohlone.edu>
2020-09-23 22:29:39 +0600

    Added UserManager app in settings.py

--- Animal_Rescue_And_Adoption_System/settings.py
+++ Animal_Rescue_And_Adoption_System/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'RescueFormManagement.apps.RescueformmanagementConfig'
+    'RescueFormManagement.apps.RescueformmanagementConfig',
+    'UserManager.apps.UsermanagerConfig',
 ]
 
 MIDDLEWARE = [
